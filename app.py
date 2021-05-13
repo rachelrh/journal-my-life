716cad71812c9ed293b86b4d05368d7c0df99073
rachelrh <ho.rachel227@gmail.com>
2021-05-13 19:18:45 -0400

    deployment

--- app.py
+++ app.py
@@ -230,6 +230,6 @@ def upload(post_id):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = os.environ.get("PORT", 5000)
+    app.run(host="0.0.0.0", port=port)
 
